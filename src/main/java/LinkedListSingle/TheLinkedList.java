5955b78a51d1f902c9d3ebb4b2c4e5edaf3bdeb6
Erich Sickert <esickert@gmail.com>
2018-03-05 16:57:20 -0800

    plus

--- src/main/java/LinkedListSingle/TheLinkedList.java
+++ src/main/java/LinkedListSingle/TheLinkedList.java
@@ -3,7 +3,8 @@ package LinkedListSingle;
 public class TheLinkedList {
     public static void main(String[] args)  {
 
-
+        LinkedListMethods list = new LinkedListMethods();
+        list.length();
 
     }
 }
