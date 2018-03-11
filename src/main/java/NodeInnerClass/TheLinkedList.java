91bce1c7ee6fb43c64685818888cadf5bac5b48f
Erich Sickert <esickert@gmail.com>
2018-03-11 14:24:33 -0700

    plus

--- src/main/java/NodeInnerClass/TheLinkedList.java
+++ src/main/java/NodeInnerClass/TheLinkedList.java
@@ -5,13 +5,23 @@ public class TheLinkedList {
 
         IntLinkedList aList = new IntLinkedList();
 
-        System.out.println(aList.length());
+
+        System.out.println("The length of the list is " + aList.length());
         for(int i = 0; i <= 5; i++) {
             aList.addNode(i);
 //            System.out.println(i);
         }
+        int length = aList.length();
+        int[] test = new int[length];
 
-        System.out.println(aList.length());
+        test = aList.toArray();
+        System.out.println("The length of the list is a " + aList.length());
+        aList.printList();
+
+        for(int temp: test)
+            System.out.print("  " + temp);
+
+
 
     }
 }
