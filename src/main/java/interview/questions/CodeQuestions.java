b547decea73b980ae51e9a29275c78498bfa116b
Erich Sickert <esickert@gmail.com>
2018-04-14 15:56:44 -0700

    plus

--- src/main/java/interview/questions/CodeQuestions.java
+++ src/main/java/interview/questions/CodeQuestions.java
@@ -1,6 +1,8 @@
 package interview.questions;
 
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -9,72 +11,75 @@ import java.util.Set;
 
 public class CodeQuestions {
 
-@Test
-public void fibonacci() {
-    int x = 0;
-    int y = 1;
-    int z;
-    int count = 10;
-    System.out.print(x + " " + y);
+    @Test
+    public void fibonacci() {
+        int x = 0;
+        int y = 1;
+        int z;
+        int count = 10;
+        System.out.print(x + " " + y);
 
-    for(int i = 2; i <= count; i++) {
-        z = x + y;
-        System.out.print(" " + z);
-        x = y;
-        y = z;
-    }
-    System.out.println("\nThe Fibonacci Sequence");
-}
-
-@Test
-public void fibonacciRecursive()    {
-    int number = 11;
-    for(int i = 0; i <= number; i++) {
-        System.out.print(fib(i) + " ");
-    }
-}
-public int fib(int num)   {
-    if ((num == 0)||(num == 1))
-        return num;
-    else
-        return (fib(num - 1)+ fib(num - 2));
-}
-
-//*******************************************************************
-@Test
-public void palandrome()    {
-    String x = "abcdcba";
-    String temp = "";
-    for(int i = x.length()-1; i >= 0; i--)  {
-        temp = temp + x.substring(i, i+1);
-    }
-    if ( temp.equals(x))
-        System.out.println(x + " is a palindrome!!!!");
-    else
-        System.out.println("NOT!!!");
-
-
-}
-
-@Test
-public void aList() {
-
-    List<String> aList = new ArrayList<String>();
-    aList.add("dog");
-    aList.add("cat");
-    for(int i = 0; i <= 2; i++) {
-        aList.add("bird");
-    }
-    for(String temp:aList)  {
-        System.out.print(temp + " ");
+        for (int i = 2; i <= count; i++) {
+            z = x + y;
+            System.out.print(" " + z);
+            x = y;
+            y = z;
+        }
+        System.out.println("\nThe Fibonacci Sequence");
     }
 
-    Set<String> aSet = new HashSet<String>(aList);
-    System.out.println();
-    for(String temp: aSet)  {
-        System.out.print(temp + " ");
+    @Test
+    public void fibonacciRecursive() {
+        int number = 11;
+        for (int i = 0; i <= number; i++) {
+            System.out.print(fib(i) + " ");
+        }
     }
 
-}
+    public int fib(int num) {
+        if ((num == 0) || (num == 1))
+            return num;
+        else
+            return (fib(num - 1) + fib(num - 2));
+    }
+
+    //*******************************************************************
+    @Test
+    public void palandrome() {
+        String x = "abcdcba";
+        String temp = "";
+        for (int i = x.length() - 1; i >= 0; i--) {
+            temp = temp + x.substring(i, i + 1);
+        }
+        if (temp.equals(x))
+            System.out.println(x + " is a palindrome!!!!");
+        else
+            System.out.println("NOT!!!");
+
+
+    }
+
+    @Test
+    public void aList() {
+
+        List<String> aList = new ArrayList<String>();
+        aList.add("dog");
+        aList.add("cat");
+        for (int i = 0; i <= 2; i++) {
+            aList.add("bird");
+        }
+        for (String temp : aList) {
+            System.out.print(temp + " ");
+        }
+
+        Set<String> aSet = new HashSet<String>(aList);
+        System.out.println();
+        for (String temp : aSet) {
+            System.out.print(temp + " ");
+        }
+
+    }
+
+
 
 } //end of class CodeQuestions
