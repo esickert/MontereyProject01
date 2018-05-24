0adbf54effab39a94a80bb2f7d8826e7f923924e
Erich Sickert <esickert@gmail.com>
2018-05-23 17:46:46 -0700

    plus

--- src/main/java/seleniumTestEnv/capabilities.java
+++ src/main/java/seleniumTestEnv/capabilities.java
@@ -1,11 +1,16 @@
 package seleniumTestEnv;
 
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,6 +31,20 @@ public class capabilities {
         WebDriver driver = new FirefoxDriver(capabilities);
 
         driver.get("https://www.google.com");
+        driver.manage().window().maximize();
+
+        // Take screenshot and store as a file format
+        File src= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        try {
+            // now copy the  screenshot to desired location using copyFile //method
+            FileUtils.copyFile(src, new File("C:/tmp/error.png"));
+        }
+
+        catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+
+        }
 
     }
 }
