6efc0a1316224521ad8b3c2b92fd362cfabd4d10
Erich Sickert <esickert@gmail.com>
2018-06-23 16:52:41 -0700

    plus

--- src/main/java/playground.java
+++ src/main/java/playground.java
@@ -2,6 +2,22 @@ import org.junit.Test;
 
 public class playground {
 
+@Test
+    public void factorial() {
+
+        int num = 5;
+        int temp = 1;
+
+        while (num > 0) {
+            temp = temp *(num--);
+            System.out.println(temp);
+
+
+        }
+        System.out.println(temp);
+
+    }
+
 
 
 
