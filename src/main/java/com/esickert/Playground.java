720519c96285f02496f47d5dbda5de7035c0cc5d
Erich Sickert <esickert@gmail.com>
2018-06-24 10:47:15 -0700

    plus

--- src/main/java/com/esickert/Playground.java
+++ src/main/java/com/esickert/Playground.java
@@ -1,8 +1,25 @@
 package com.esickert;
 
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Playground {
+
+@Test
+    public void palindrome() {
+    String word = "abcdcbaz";
+    String temp = "";
+    int front = (word.length() - 1) / 2;
+    int back = word.length() - 1;
+    for (int i = word.length() - 1; i >= 0; i--) {
+        temp = temp + word.substring(i, i + 1);
+        System.out.println(temp);
+    }
+    if (word.equals(temp))
+        System.out.println("\nPalindrome");
+    else
+        System.out.println("\nNOT!!!!!!!!!!!!!!");
+}
+}
+
+
 
