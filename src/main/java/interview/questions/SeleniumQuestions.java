c5f792b0d15c9826ed2bf00dc7a70cab7a684771
Erich Sickert <esickert@gmail.com>
2018-07-24 11:36:05 -0700

    plus

---
+++ src/main/java/interview/questions/SeleniumQuestions.java
@@ -0,0 +1,38 @@
+package interview.questions;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleniumQuestions {
+
+
+    public WebDriver fireFoxDriver() {
+        System.setProperty("webdriver.gecko.driver", "C://SeleniumDrivers//geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        return driver;
+    }
+
+    public WebDriver chromeDriver() {
+        System.setProperty("webdriver.chrome.driver","C://SeleniumDrivers/chromeDriver.exe");
+        WebDriver driver = new ChromeDriver();
+        return driver;
+
+    }
+
+    @Test
+    public void openFireFox() {
+
+        fireFoxDriver().get("http://www.google.com");
+    }
+
+    @Test
+    public void openChrome()    {
+
+        chromeDriver().get("http://www.yahoo.com");
+    }
+
+}
+
+
