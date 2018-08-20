9c9ccdf8049acb4ad192d69acf3ea7cbb827694e
Erich Sickert <esickert@gmail.com>
2018-08-20 15:34:11 -0700

    plus

--- src/main/java/com/esickert/Selenium2.java
+++ src/main/java/com/esickert/Selenium2.java
@@ -1,7 +1,9 @@
 package com.esickert;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
@@ -19,6 +21,8 @@ public class Selenium2 {
         driver.manage().window().maximize();
 
         Actions build = new Actions(driver);
+        WebElement element = driver.findElement(By.cssSelector("#hptl > a:nth-child(2)"));
+        build.doubleClick(element).perform();
         //and some more....
     }
 }
