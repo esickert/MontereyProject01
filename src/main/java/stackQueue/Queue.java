e8caabbf0a6c5cf1f1c7ddbd221a57e10e9ebb59
Erich Sickert <esickert@gmail.com>
2018-08-26 16:24:03 -0700

    plus

---
+++ src/main/java/stackQueue/Queue.java
@@ -0,0 +1,24 @@
+package stackQueue;
+
+import java.util.Stack;
+
+public class Queue {
+
+    Queue() {
+    }
+
+    Stack<Integer> stk1 = new Stack<>();
+    Stack<Integer> stk2 = new Stack<>();
+
+    public boolean isEmpty()    {
+        return ((stk1.empty())&&(stk2.empty()));
+    } //end of method isEmpty
+
+    public int size()   {
+        return stk1.size() + stk2.size();
+    }
+
+    public void queue() {
+        if 
+    }
+}
