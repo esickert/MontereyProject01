f8fc315e45c67c179fda4fba4bc0841ec92f7913
Erich Sickert <esickert@gmail.com>
2018-08-28 15:17:57 -0700

    plus

--- src/main/java/com/esickert/Selenium2.java
+++ src/main/java/com/esickert/Selenium2.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 
 public class Selenium2 {
@@ -15,7 +16,7 @@ public class Selenium2 {
         // login to Monster website
         System.setProperty("webdriver.chrome.driver", "c://SeleniumDrivers//chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-//        driver.get("https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select");   
+//        driver.get("https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select");
         driver.get("https://www.google.com");
 
         driver.manage().window().maximize();
@@ -23,7 +24,12 @@ public class Selenium2 {
         Actions build = new Actions(driver);
         WebElement element = driver.findElement(By.cssSelector("#gbwa > div.gb_Pc > a")); //not sure what this is clicking on?
  //       element.click();
-        build.doubleClick(element).perform();
+        WebElement element2 = driver.findElement(By.cssSelector("#gb36 > span.gb_W"));
+        build.click(element)
+             .click()
+             .contextClick()
+             .click(element2)
+             .perform();
         //and some more...
     }
 }
