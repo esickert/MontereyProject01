0ee839523af3cc0276d49a73041a799613e7de45
Erich Sickert <esickert@gmail.com>
2018-09-14 13:13:53 -0700

    plus

--- src/main/java/selenium/practice/Practice.java
+++ src/main/java/selenium/practice/Practice.java
@@ -1,9 +1,24 @@
 package selenium.practice;
 
-import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-@Test
+import java.io.IOException;
+
 public class Practice   {
+    public static void main(String[] erich) throws InterruptedException {
+
+        System.setProperty("webdriver.gecko.driver", "c://SeleniumDrivers//geckodriver.exe");
+        WebDriver driver;
+        driver = new FirefoxDriver();
+
+        driver.navigate().to("http://www.yahoo.com");
+
+        Thread.sleep(5000);
+        driver.quit();
+
+
+    }
 
 
 }
