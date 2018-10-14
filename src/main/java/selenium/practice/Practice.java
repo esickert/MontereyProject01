26ce290fe805136b034c2ae19cac00d13a0e5bd7
Erich Sickert <esickert@gmail.com>
2018-10-14 10:24:08 -0700

    plus

--- src/main/java/selenium/practice/Practice.java
+++ src/main/java/selenium/practice/Practice.java
@@ -1,7 +1,25 @@
 package selenium.practice;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Practice   {
     public static void main(String[] eric)  {
+
+        System.setProperty("webdriver.chrome.driver","c://SeleniumDrivers//chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.yahoo.com");
+
+        driver.manage().window().maximize();
+
+        try {
+            Thread.sleep(5000);
+        }
+        catch(Exception e)   {
+            e.printStackTrace();
+        }
+
+        driver.close();
 
 
     }
