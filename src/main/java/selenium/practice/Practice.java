a1251fb185f938e4d8a8c1b06263e8b3229a6f53
Erich Sickert <esickert@gmail.com>
2018-10-21 14:25:08 -0700

    plus

--- src/main/java/selenium/practice/Practice.java
+++ src/main/java/selenium/practice/Practice.java
@@ -1,21 +1,8 @@
-package selenium.practice;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
 public class Practice   {
-    public static void main(String[] eric)  {
+    public static void main(String[] erich) {
 
-        System.setProperty("webdriver.gecko.driver", "c://SeleniumDrivers//geckodriver.exe");
-        WebDriver driver = new FirefoxDriver();
 
-        driver.get("http://www.yahoo.com");
-        WebElement point = driver.findElement(By.cssSelector("#uh-signin"));
-        point.click();
-
+        
     }
 }
 
