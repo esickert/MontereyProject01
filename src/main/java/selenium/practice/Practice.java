dcba2e932cfeef1a845b311cb8db9b770b7e64f5
Erich Sickert <esickert@gmail.com>
2018-10-30 16:43:20 -0700

    plus

--- src/main/java/selenium/practice/Practice.java
+++ src/main/java/selenium/practice/Practice.java
@@ -9,11 +9,11 @@ public class Practice   {
         System.setProperty("webdriver.gecko.driver","c://SeleniumDrivers//geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
         driver.get("http://www.yahoo.com");
- //       WebElement stuff = driver.findElement(By.cssSelector("#yui_3_18_0_3_1540854298580_818"));
+      WebElement stuff = driver.findElement(By.cssSelector("#yui_3_18_0_3_1540854298580_818"));
  //       stuff.click();
 
 
     }
 }
 
-//*********
+//************
