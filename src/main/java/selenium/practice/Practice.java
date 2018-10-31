4abae43b1635c32e67a3c7a58b14e23cdd928291
Erich Sickert <esickert@gmail.com>
2018-10-31 14:50:52 -0700

    plus

--- src/main/java/selenium/practice/Practice.java
+++ src/main/java/selenium/practice/Practice.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 ;
-//NOTE: THIS SELENIUM SI SETUP TO RUN IN LINUX 14.04
+//NOTE: THIS SELENIUM IS SETUP TO RUN IN LINUX 14.04
 public class Practice   {
     public static void main(String[] erich) {
 
