2167e399d6667b9d5967f0d8000d7fe9a208623e
Erich Sickert <esickert@gmail.com>
2018-12-01 13:34:20 -0800

    plus

--- src/main/java/selenium/practice/Practice.java
+++ src/main/java/selenium/practice/Practice.java
@@ -1,23 +1,2 @@
 package selenium.practice;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-;
-//NOTE: THIS SELENIUM IS SETUP TO RUN IN LINUX 14.04
-public class Practice   {
-    public static void main(String[] erich) {
-
-        System.setProperty("webdriver.chrome.driver","c://SeleniumDrivers//chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://www.yahoo.com");
-
-        WebElement stuff = driver.findElement(By.cssSelector("#yui_3_18_0_3_1540854298580_818"));
- //       stuff.click();
-
-
-    }
-}
-
-//***
