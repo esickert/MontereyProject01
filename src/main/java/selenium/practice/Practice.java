25b669bfece9d8573e19f257ca1a4bb812cd76d3
Erich Sickert <esickert@gmail.com>
2018-12-01 19:05:37 -0800

    plus

--- src/main/java/selenium/practice/Practice.java
+++ src/main/java/selenium/practice/Practice.java
@@ -1,2 +1,21 @@
 package selenium.practice;
 
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Practice   {
+
+    @Test
+    public void testSelenium()  {
+
+        System.setProperty("webdriver.gecko.driver", "c://SeleniumDrivers//geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("http://www.yahoo.com");
+    }
+
+}
+
+
+
