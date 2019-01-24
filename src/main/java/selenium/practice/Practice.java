53e3fd84202547875576392937f005fd7a8fa762
Erich Sickert <esickert@gmail.com>
2019-01-23 16:19:38 -0800

    plus

--- src/main/java/selenium/practice/Practice.java
+++ src/main/java/selenium/practice/Practice.java
@@ -1,40 +1,16 @@
 package selenium.practice;
 
+
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.server.handler.FindElement;
 
 public class Practice   {
 
-    @Test
-    public void testSelenium()  {
 
-        System.setProperty("webdriver.chrome.driver", "c://SeleniumDrivers//chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-
-        findYahoo(driver);
-//        login(driver);
 
  //*************************************************************************
-    }
-
-    public void findYahoo(WebDriver x)  {
-
-        x.get("http://www.yahoo.com");
-        x.manage().window().maximize();
-        WebElement mail = x.findElement(By.cssSelector("#uh-mail-link"));
-        mail.click();
-    }
-
-/*   public void login(Webdriver y)  {
-        WebElement login = y.findElement(By.cssSelector())
-
-    }  */
 }
+
+
 
 
 
