abf51f9ef8ea0a3dd7205e74ca7b7bd7768c65c4
Erich Sickert <esickert@gmail.com>
2019-01-23 16:33:58 -0800

    plus

--- src/main/java/selenium/practice/Practice.java
+++ src/main/java/selenium/practice/Practice.java
@@ -2,8 +2,20 @@ package selenium.practice;
 
 
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Practice   {
+
+@Test
+    public void yahoo() {
+
+    System.setProperty("webdriver.chrome.driver","c://SeleniumDrivers//chromedriver.exe");
+    WebDriver driver = new ChromeDriver();
+    driver.get("http://www.yahoo.com");
+
+
+    }
 
 
 
