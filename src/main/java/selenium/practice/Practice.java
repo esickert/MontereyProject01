363a32e2cb5a7120aa6df3d441543731f2e349e9
Erich Sickert <esickert@gmail.com>
2019-02-10 11:59:46 -0800

    plus

--- src/main/java/selenium/practice/Practice.java
+++ src/main/java/selenium/practice/Practice.java
@@ -4,15 +4,19 @@ package selenium.practice;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Practice   {
 
 @Test
     public void yahoo() {
 
-    System.setProperty("webdriver.chrome.driver","c://SeleniumDrivers//chromedriver.exe");
-    WebDriver driver = new ChromeDriver();
-    driver.get("http://www.yahoo.com");
+ //   System.setProperty("webdriver.chrome.driver","c://SeleniumDrivers//chromedriver.exe");
+ //   WebDriver driver = new ChromeDriver();
+
+    System.setProperty("webdriver.gecko.driver", "c:\\SeleniumDrivers\\geckodriver.exe");
+    WebDriver driver = new FirefoxDriver();
+    driver.navigate().to("http://www.yahoo.com");
 
 
     }
