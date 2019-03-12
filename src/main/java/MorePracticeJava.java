67be45eb1cfd5c12f386c700cc17acd9d1033abf
Erich Sickert <esickert@gmail.com>
2019-03-11 18:47:08 -0700

    plus

--- src/main/java/MorePracticeJava.java
+++ src/main/java/MorePracticeJava.java
@@ -1,10 +1,30 @@
 import org.junit.Test;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MorePracticeJava   {
 
 @Test
 public void Test()  {
     //***************************************
     System.out.println("Hello World");
+}
+
+@Test
+    public void addPairs() {
+
+    int[] a = {3, 5, 1, 2, 7, 6, 8, 5, 9, 66, 55, 4, 5, 2, 8};
+    int sum = 17;
+
+    Map<Integer, Integer> pairs = new HashMap<>();
+
+    for (int i = 0; i <= a.length-1; i++) {
+        if (pairs.containsKey(sum - a[i]))
+            System.out.println("(" + a[i] + ":" + (sum - a[i]) + "}");
+        else
+            pairs.put(a[i], 0);
+    }
 }
 }
