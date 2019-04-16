f7bc6f6f17c8bab475566c7de0810c790786b0ef
Erich Sickert <esickert@gmail.com>
2019-04-15 22:33:27 -0700

    plus

--- src/main/java/interview/questions/CodeQuestions.java
+++ src/main/java/interview/questions/CodeQuestions.java
@@ -153,7 +153,7 @@ import static org.testng.AssertJUnit.assertEquals;
 
         }
 
-        //**************************************************************
+        //*****************************************************************
         //find the largest (or smallest) number in the array)
         @Test
         public void findLargestMethod() {
