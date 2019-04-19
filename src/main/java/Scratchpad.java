c16b357303adfb9f6e5a445fc88c1b8a32a50874
Erich Sickert <esickert@gmail.com>
2019-04-18 21:02:58 -0700

    plus

---
+++ src/main/java/Scratchpad.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+public class Scratchpad {
+
+    @Test
+    public void Switch()    {
+
+        int a = 0;
+        int b = 999;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("This is a now: " + a);
+        System.out.println("This is b now: " + b);
+    }
+
+    @Test
+}
