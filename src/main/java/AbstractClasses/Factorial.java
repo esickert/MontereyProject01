0c7ba20eb92bdd3e483109b5c859c1c90f3b4089
Erich Sickert <esickert@gmail.com>
2019-05-30 18:40:53 -0700

    plus

--- src/main/java/AbstractClasses/Factorial.java
+++ src/main/java/AbstractClasses/Factorial.java
@@ -2,6 +2,8 @@ package AbstractClasses;
 
 import org.testng.annotations.Test;
 
+import static interview.questions.CodeQuestions.fib;
+
 @Test
 public class Factorial {
 
@@ -14,7 +16,49 @@ public class Factorial {
         System.out.println(temp);
 
     }
-}
+
+    public void fac2()  {
+    int num = 5;
+    int temp = 1;
+ //   when the inequlities hit 0 it will 0 out temp
+    while (num > 0) {
+        temp = temp * num--;
+        System.out.println(temp);
+    }
+    System.out.println(temp);
+    }
+
+    public void fib1()  {
+        for(int i = 0; i <= 10; i++) {
+            System.out.print(fib(i) + " ");
+        }
+    }
+
+    public int fib(int x)   {
+        if ((x == 0)|| (x ==1))
+            return x;
+        else
+            return (fib(x-1)+fib(x-2));
+    }
+
+    public void fib2()  {
+
+        int a = 0;
+        int b = 1;
+        int c;
+        System.out.println(a + " " + b);
+        for(int i = 2; i <= 10; i++) {
+            c = a + b;
+            System.out.print(" " + c);
+            a = b;
+            b = c;
+        }
+        }
+
+    }
+
+
+
 
 
 
