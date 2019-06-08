35f68552ba14d31614e09df5b28c12d4c475a133
Erich Sickert <esickert@gmail.com>
2019-06-07 23:43:19 -0700

    plus

--- src/main/java/interview/questions/CodeQuestions.java
+++ src/main/java/interview/questions/CodeQuestions.java
@@ -589,7 +589,7 @@ import static org.testng.AssertJUnit.assertEquals;
 
         int[] a = {2, 5, 4, 7, 2, 3, 1, 5, 6, 8, 5, 3, 4};
         int x = 1;
-        Arrays.sort(a);  //sorts array
+        Arrays.sort(a);                      //sorts array
         for (int temp : a) {
  //           System.out.print(temp + " ");
             while (x != 10) {
