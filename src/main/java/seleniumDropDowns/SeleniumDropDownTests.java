ee74a95c4c8453be46553ee9e9ad5a322c0c1f6b
Erich Sickert <esickert@gmail.com>
2019-07-23 23:30:59 -0700

    plus

--- src/main/java/seleniumDropDowns/SeleniumDropDownTests.java
+++ src/main/java/seleniumDropDowns/SeleniumDropDownTests.java
@@ -71,7 +71,7 @@ public class SeleniumDropDownTests {
 
     }
 
-    @Test
+    @Test   //******************************************************************
     public void test_ul_li()    {
 
         System.setProperty("webdriver.chrome.driver", "c://SeleniumDrivers//chromedriver.exe");
