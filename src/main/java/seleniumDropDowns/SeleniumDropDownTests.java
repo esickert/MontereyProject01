0059aa75653f3b175f6e809edb7c598194d9b05c
Erich Sickert <esickert@gmail.com>
2019-07-30 23:34:50 -0700

    plus

--- src/main/java/seleniumDropDowns/SeleniumDropDownTests.java
+++ src/main/java/seleniumDropDowns/SeleniumDropDownTests.java
@@ -18,23 +18,23 @@ import static org.openqa.selenium.By.xpath;
 public class SeleniumDropDownTests {
 
     @Test  //this is the use of the select class from selenium. this is so stupid!!!!!!!!!
-    public void TestSelect()  {
+    public void TestSelect() {
 
-    // login to Monster website
+        // login to Monster website
         System.setProperty("webdriver.chrome.driver", "c:\\SeleniumDrivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select");
 //        driver.get("http:esickert.github.io");
         driver.manage().window().maximize();
 
-    //open and select options in dropdown menu using the Select class.
+        //open and select options in dropdown menu using the Select class.
 
-     WebElement dropdown = driver.findElement(By.xpath("//*[@id=\"wikiArticle\"]/p[14]/select"));
+        WebElement dropdown = driver.findElement(By.xpath("//*[@id=\"wikiArticle\"]/p[14]/select"));
         Select selectOption = new Select(dropdown);      //to use selenium Select class the tags need to be under the select tag
 /** org.openqa.selenium.support.ui.UnexpectedTagNameException: Element should have been "select" but was "ul"
-        Build info: version: '3.4.0', revision: 'unknown', time: 'unknown'
-        System info: host: 'TENOCHTITLAN', ip: '192.168.1.100', os.name: 'Windows 7', os.arch: 'amd64', os.version: '6.1', java.version: '1.8.0_144'
-       Driver info: driver.version: unknown  **/
+ Build info: version: '3.4.0', revision: 'unknown', time: 'unknown'
+ System info: host: 'TENOCHTITLAN', ip: '192.168.1.100', os.name: 'Windows 7', os.arch: 'amd64', os.version: '6.1', java.version: '1.8.0_144'
+ Driver info: driver.version: unknown  **/
 
         //#wikiArticle > p:nth-child(23) > select
 
@@ -50,31 +50,27 @@ public class SeleniumDropDownTests {
             Thread.sleep(2000);
             drop.selectByVisibleText("First Value");
 
-        }
-        catch(InterruptedException e)
-        {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
         WebElement x = drop.getFirstSelectedOption();
-        System.out.println("Line 68: " +  x.getText());
+        System.out.println("Line 68: " + x.getText());
         Assert.assertEquals(x.getText(), "First Value");
         try {
             Thread.sleep(3000);
-        }
-        catch(InterruptedException e)
-        {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
 
-     driver.close();
+        driver.close();
 
     }
 
     //****************************************************************************
     //For some reasone Indians love to ask about the Select class when most of the dropdowns are li.
     @Test
-    public void test_ul_li()    {
+    public void test_ul_li() {
 
         System.setProperty("webdriver.chrome.driver", "c://SeleniumDrivers//chromedriver.exe");
         WebDriver driver = new ChromeDriver();
@@ -89,21 +85,20 @@ public class SeleniumDropDownTests {
 
         driver.findElement(By.cssSelector("#mobile-navbar-search > ul > li > a")).click();
         List<WebElement> pulldownOptions = driver.findElements(By.xpath("//ul[@class='dropdown-menu']/li"));
-     // Loop through the options and select the one that matches
+        // Loop through the options and select the one that matches
 
-        for (WebElement opt : pulldownOptions ) {
+        for (WebElement opt : pulldownOptions) {
             System.out.println("The option is " + opt.getText());
         }
         pulldownOptions.get(7).click();   //this worked!!!!!  smiley face.*****
 
-        try{
+        try {
             Thread.sleep(3000);
-        }
-        catch(InterruptedException e)   {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
- //       driver.close();
+        //       driver.close();
     }
-    }
+}  // end of Sellenium
 
 
