258a0fb3f19a50f6506b6b3d15fb620d1b44d153
Erich Sickert <esickert@gmail.com>
2019-08-06 18:33:53 -0700

    plus

--- src/main/java/com/sketchpad/sketchPad.java
+++ src/main/java/com/sketchpad/sketchPad.java
@@ -1,12 +1,35 @@
 package com.sketchpad;
 
 import org.junit.Test;
+import java.util.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Scanner;
 
 public class sketchPad {
 
     @Test
-    public void Test()  {
-        System.out.println("This is a test");
+    public void javaIO() {
+        String file = "C://Temp//erik.txt";
+        File filename = new File(file);
+        PrintStream stuff = null;
+        try {
+            stuff = new PrintStream(filename);
+            stuff.println("hello there");
+        } catch (IOException e) {
+            System.out.println("ERROR");
+            e.printStackTrace();
+        }
+
+        Scanner scan = new Scanner(stuff);
+        while (scan.hasNextLine()) {
+            String text = scan.nextLine();
+            System.out.println(text);
+        }
+
+
     }
 }
-}
+
