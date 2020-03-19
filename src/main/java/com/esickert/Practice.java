49e45add6bc2c709797f6516ee48469e3f49a779
Erich Sickert <esickert@gmail.com>
2020-03-18 18:19:22 -0700

    plus

---
+++ src/main/java/com/esickert/Practice.java
@@ -0,0 +1,14 @@
+package com.esickert;
+
+import org.junit.Test;
+
+public class Practice {
+
+    @Test
+    public void check() {
+        System.out.println("This is for practice coding");
+
+    }
+
+    //****************************************************************************************************************
+}
