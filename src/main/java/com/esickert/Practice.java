8a69b0285b30862046349531ff49b5a1b38538ac
Erich Sickert <esickert2@gmail.com>
2020-12-03 14:23:13 -0800

    plus

--- src/main/java/com/esickert/Practice.java
+++ src/main/java/com/esickert/Practice.java
@@ -10,4 +10,35 @@ public class Practice {
 
     }
     //*************************************************************************************
+
+    @Test
+    public void palidrome() {
+        String word = "abcdcba";
+        String temp = "";  //remember this will add a space, space is a character!!!!;
+
+        for(int i = word.length()-1; i >= 0; i--) {
+            temp = temp + word.charAt(i);
+            System.out.println(temp);
+        }
+        System.out.println(word.equals(temp));
+        if (word.equals(temp))
+            System.out.println("Palidrome");
+        else
+            System.out.println("NOT");
+    }
+
+    @Test
+    public void palStringBuffer()   {
+        String word = "abcdcba";
+        String temp = "";
+
+        StringBuffer str = new StringBuffer(word);
+        if(word.equals(str.reverse().toString()))
+            System.out.println("Palindrome");
+        else
+            System.out.println("not");
+    }
+    //*****************************************************************************
+
+
 }
