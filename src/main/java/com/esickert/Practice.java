523f1936b44a1e3ce77b83fe07ce1182e7923916
Erich Sickert <esickert2@gmail.com>
2020-12-05 12:47:57 -0800

    plus

--- src/main/java/com/esickert/Practice.java
+++ src/main/java/com/esickert/Practice.java
@@ -1,77 +1,28 @@
 package com.esickert;
 
 import org.junit.Test;
-import org.openqa.selenium.remote.server.handler.DeleteSession;
-import org.testng.collections.Maps;
 
-import java.security.cert.X509Certificate;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Practice {
+public class Practice{
 
     @Test
-    public void check() {
-        System.out.println("This is for practicing my piano scales");
-
-    }
-    //*************************************************************************************
-
-    @Test
-    public void palidrome() {
-        String word = "abcdcba";
-        String temp = "";  //remember this will add a space, space is a character!!!!;
-
-        for(int i = word.length()-1; i >= 0; i--) {
-            temp = temp + word.charAt(i);
-            System.out.println(temp);
-        }
-        System.out.println(word.equals(temp));
-        if (word.equals(temp))
-            System.out.println("Palidrome");
-        else
-            System.out.println("NOT");
+    public void test()  {
+        System.out.println("Practicing my piano scales");
     }
 
     @Test
-    public void palStringBuffer()   {
-        String word = "abcdcba";
-        String temp = "";
-
-        StringBuffer str = new StringBuffer(word);
-        if(word.equals(str.reverse().toString()))
-            System.out.println("Palindrome");
-        else
-            System.out.println("not");
-    }
-    @Test
-    public void instances(){
-        int[] a = {2,2,2,2,3,4,6,7,2,1,2,4,4,4,4,5};
-        for(int i = 0; i <= 9; i++)
-            numOf(i,a);
-    }
-
-    public void numOf(int x, int[] a ) {
-        int count = 0;
-        for(int i = 0; i < a.length; i++)   {
-            if(x == a[i])
-                count++;
-        }
-        System.out.println("There are " + count + " instance of " + x);
-    }
-
-    @Test
-    public void pairs() {
-        int[] a = {2,5,1,2,4,7,6,9,3,2,4,6,9,8,7,8,6,7};
+    public void mapPairs()  {
+        int[] a = {2,5,1,2,7,5,9,7,8,5,3,4,2,1};
         int sum = 13;
 
         Map<Integer, Integer> aMap = new HashMap<>();
-        for(int i = 0; i <= a.length-1; ++i) {
+        for(int i = 0; i <= a.length-1; i++)
             if (aMap.containsKey(sum - a[i]))
-                System.out.println("(" + (sum - a[i]) + ":" + a[i] + ")");
+                System.out.println("(" + a[i] + ":" + (sum - a [i]) + ")");
             else
-                aMap.put(a[i],0);
-        }
+                aMap.put(a[i], 0);
     }
 
 
