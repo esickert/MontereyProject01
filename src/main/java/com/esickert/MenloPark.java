cbbec83586c78bc547e05af2afd1f47d1c5866ad
Erich Sickert <esickert2@gmail.com>
2020-12-07 11:11:07 -0800

    plus

---
+++ src/main/java/com/esickert/MenloPark.java
@@ -0,0 +1,13 @@
+package com.esickert;
+
+import org.junit.Test;
+
+public class MenloPark {
+
+    @Test
+    public void Testing()   {
+        System.out.println("This is for Savitch");
+
+    }
+
+}
