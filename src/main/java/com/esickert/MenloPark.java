4d68dc434d89ce0f76be1b57c0de25e3a2f5c9af
Erich Sickert <esickert2@gmail.com>
2020-12-08 14:16:44 -0800

    plus

--- src/main/java/com/esickert/MenloPark.java
+++ src/main/java/com/esickert/MenloPark.java
@@ -1,12 +1,51 @@
 package com.esickert;
 
 import org.junit.Test;
+import org.openqa.selenium.remote.server.handler.DeleteSession;
 
 public class MenloPark {
 
     @Test
     public void Testing()   {
         System.out.println("This is for Savitch");
+    }
+
+    @Test
+    public void palidrome() {
+        String word = "abcdcb";
+        String temp = "";
+
+        for(int i = word.length()-1; i >= 0; i--)  {
+            temp = temp + word.charAt(i);
+            System.out.println(temp);
+        }
+        System.out.println(word.equals(temp));
+        if (word.equals(temp))
+            System.out.println("Palindrome");
+        else
+            try {
+                System.out.println(1/0);
+            }
+            catch(Exception e) {
+                System.out.println(e + "\nNOT a palindrome");
+            }
+    }
+
+    @Test
+    public void palindromeStringBuffer()    {
+        String word = "abcdcba";
+        String temp = "";
+
+        StringBuilder str = new StringBuilder(word);
+        if (word.equals(str.reverse().toString()))
+            System.out.println("Palindrome");
+        else
+            try {
+                System.out.println(1/0);
+            }
+            catch(Exception e) {
+                System.out.println(e + "\nNOT a palindrome");
+            }
     }
 
 
