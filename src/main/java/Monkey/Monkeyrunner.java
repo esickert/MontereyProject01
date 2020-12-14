95523b77e8bc954160a085fd0c126b66e821ae11
Erich Sickert <esickert2@gmail.com>
2020-12-13 16:03:57 -0800

    plus

---
+++ src/main/java/Monkey/Monkeyrunner.java
@@ -0,0 +1,22 @@
+package Monkey;
+
+/*public class Monkeyrunner {
+
+import com.android.monkeyrunner.MonkeyDevice;
+import com.android.monkeyrunner.adb.AdbBackend;
+
+    public class Monk {
+        public static void main(String[] args) {
+            // TODO code application logic here
+            Monk monk=new Monk();
+            monk.demo();
+        }
+        public void demo()
+        {
+            AdbBackend ab = new AdbBackend();
+            MonkeyDevice device = ab.waitForConnection();
+            //Print Device Name
+            System.out.println(device.getProperty("build.model"));
+            device.dispose();
+        }
+}  */
