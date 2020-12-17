34bd07806f39cd2a6cf407554159bfb8817c9e62
Erich Sickert <esickert2@gmail.com>
2020-12-16 16:37:10 -0800

    plus

---
+++ src/main/java/SavitchPractice/SpeciesDemo.java
@@ -0,0 +1,10 @@
+package SavitchPractice;
+
+public class SpeciesDemo {
+    public static void main(String[] erich) {
+
+        Species newSpecies = new Species();
+        newSpecies.readInput();
+    }
+}
+
