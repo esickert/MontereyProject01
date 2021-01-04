d3b57d0a65baf88f9ba8ae88042a6fc3a8b165ca
Erich Sickert <esickert2@gmail.com>
2021-01-03 22:10:21 -0800

    plus

--- src/main/java/com/esickert/Practice.java
+++ src/main/java/com/esickert/Practice.java
@@ -106,7 +106,18 @@ public class Practice   {
         }
     }
 
+    @Test
+    public void fib()   {
+        for(int i = 0; i <= 10; i++)    {
+            System.out.print(fib(i) + " ");
+        }
+    }
 
-
+    public int fib(int x)  {
+        if((x == 0) || (x == 1))
+            return x;
+        else
+            return fib(x-1) + fib(x - 2);
+    }
 }
 
