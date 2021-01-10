22c3008f6647379597d662dac7bd286bc4fa65fc
Erich Sickert <esickert2@gmail.com>
2021-01-10 08:54:29 -0800

    plus

--- src/main/java/com/esickert/Practice.java
+++ src/main/java/com/esickert/Practice.java
@@ -1,4 +1,32 @@
-public class Practice {
+package com.esickert;
 
+import org.junit.Test;
+import java.io.File;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Practice  {
+
+    @Test
+    public void toFile() throws Exception   {
+        String filename = "c://tmp//erich.txt";
+
+        PrintStream in = new PrintStream(filename);
+        in.println("Cold hearted orb that rules the night");
+        in.close();
+
+        Scanner out = new Scanner(new File(filename));
+
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
