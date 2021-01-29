30c67b4f5ceb373e35c62cfc68f1a1de49118818
Erich Sickert <esickert2@gmail.com>
2021-01-29 07:02:03 -0800

    plus

--- src/main/java/com/esickert/Practice.java
+++ src/main/java/com/esickert/Practice.java
@@ -27,9 +27,14 @@ public class Practice  {
   @Test
   public void Occurances()  {
     int[] a = {2,4,5,2,1,7,6,6,6,6,6,4,4,4,4,9,9,9,9};
-    int temp = 0;
+    int temp = 2;
+    int count = 0;
 
-
+    for(int i = 0; i <= a.length - 1; i++) {
+      if (a[i] == temp)
+        count++;
+    }
+    System.out.println("There are " + count + " of " + temp + "'s");
   }
 
 }
