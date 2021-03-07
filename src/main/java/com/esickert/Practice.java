3074cd23d9dbad118c1049b857b74e9e2ae03e38
esickert <esickert2@gmail.com>
2021-03-06 16:38:10 -0800

    plus

--- src/main/java/com/esickert/Practice.java
+++ src/main/java/com/esickert/Practice.java
@@ -7,33 +7,6 @@ public class Practice {
     System.out.println("HELLO WORLD");
   }
 
-  @Test
-  public void palindrome()  {
-    String word = "abcdcba";
-    String temp = "";
-    for(int i = word.length()-1; i >= 0; i--) {
-      temp = temp + word.charAt(i);
-      System.out.println(temp);
-    }
-    if (word.equals(temp))
-      System.out.println("PALINDROME");
-    else
-      System.out.println("NOT!!!");
-  }
-  //*****************************************************************
-  @Test
-  public void palindromeStringBuffer()  {
-    String word = "abcdcba";
-    String temp = "";
-    StringBuffer str = new StringBuffer(word);
-    if(str.reverse().toString().equals(word))
-      System.out.println("palidrome");
-    else
-      System.out.println("NOT");
-  }
-  //*****************************************************************
-  @Test
-  public void printToFile() {
 
 
 }
