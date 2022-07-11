63723d517555e7345ea2010f100e965205f908c6
esickert <esickert2@gmail.com>
2022-07-11 03:08:00 -0700

    plus

--- src/main/java/com/esickert/Practice2.java
+++ src/main/java/com/esickert/Practice2.java
@@ -7,5 +7,6 @@ public class Practice2 {
     @Test
     public void printme()  {
         System.out.println("This is a test");
+        System.out.println("Of the emergency broadcast system.");
     }
 }
