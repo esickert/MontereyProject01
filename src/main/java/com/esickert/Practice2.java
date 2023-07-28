b2bbb242f60da5cad2c4b98d3f87356edb38aec1
esickert <esickert2@gmail.com>
2023-07-28 10:40:01 -0700

    plus

--- src/main/java/com/esickert/Practice2.java
+++ src/main/java/com/esickert/Practice2.java
@@ -6,7 +6,7 @@ public class Practice2 {
 
     @Test
     public void printme()  {
-        System.out.println("This is a test");
+        System.out.println(" This is a test");
         System.out.println("Of the emergency broadcast system.");
     }
     //********************************************************
@@ -27,6 +27,6 @@ public class Practice2 {
     //********************************************************
     @Test
     public void fibinacciRecursive()   {
-        System.out.println("This is fibinacci recursive");
+        System.out.println(" This is fibinacci recursive");
     }
 }
