7afc05b5685dfccdfa14ee4ccf55aa875ae148de
esickert <esickert2@gmail.com>
2024-02-16 00:46:17 -0800

    plus

--- src/main/java/com/esickert/Practice.java
+++ src/main/java/com/esickert/Practice.java
@@ -278,6 +278,26 @@ public class Practice {
       System.out.println(testMe.isEmpty());
 
     }
+
+ /*
+    @Test
+    import java.io.*;
+
+    public void CmdTest {
+    //    public static void main(String[] args) throws Exception {
+            ProcessBuilder builder = new ProcessBuilder(
+                    "cmd.exe", "/c", "cd \"C:\\Program Files\\Microsoft SQL Server\" && dir");
+            builder.redirectErrorStream(true);
+            Process p = builder.start();
+            BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line;
+            while (true) {
+                line = r.readLine();
+                if (line == null) { break; }
+                System.out.println(line);
+            }
+        }
+    }     */
 }
 
 
