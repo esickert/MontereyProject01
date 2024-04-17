190191db3679b42ea7879245acd4daeef4defb46
esickert <esickert2@gmail.com>
2024-04-16 17:25:15 -0700

    plus

--- src/main/java/com/esickert/Practice.java
+++ src/main/java/com/esickert/Practice.java
@@ -29,7 +29,7 @@ public class Practice {
   public void helloWorld()  {System.out.println("\n Hello world. We are using junit and maven");
   }
 
-  //****************************************************
+  //**************************************************
   // *
 
   @Test
